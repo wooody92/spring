7e049a89ad154130836aa4678403e3f3b4438b77
wooody92 <pkower4@naver.com>
2020-08-03 17:27:10 +0900

    DAY6 - Ioc 컨테이너 7부

--- spring-study/src/main/java/dev/spring/framework/springstudy/Scope/AppRunner.java
+++ spring-study/src/main/java/dev/spring/framework/springstudy/Scope/AppRunner.java
@@ -1,31 +1,24 @@
 package dev.spring.framework.springstudy.Scope;
 
-import dev.spring.framework.springstudy.Environment.StudyRepository;
-import java.util.Arrays;
+import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationContext ctx;
-
-    // 방법 1
-    @Value("${app.name}")
-    String appName;
+    MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ctx.getEnvironment();
-        // 방법 1
-        System.out.println(appName);
-        // 방법 2
-        System.out.println(environment.getProperty("app.name"));
+        System.out.println(messageSource.getClass());
+        System.out.println(messageSource.getMessage("greeting", new String[]{"Henry"}, Locale.getDefault()));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"Henry"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"Henry"}, Locale.CANADA));
     }
+    
 }

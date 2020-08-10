1bc76c780fc68e956f6841a89bb32d42accc7d29
wooody92 <pkower4@naver.com>
2020-08-10 20:59:17 +0900

    DAY13 - Spring Expression Language

--- spring-study/src/main/java/dev/spring/framework/springstudy/AppRunner.java
+++ spring-study/src/main/java/dev/spring/framework/springstudy/AppRunner.java
@@ -3,8 +3,12 @@ package dev.spring.framework.springstudy;
 import dev.spring.framework.springstudy.validate.Event;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
@@ -13,26 +17,44 @@ import org.springframework.validation.Validator;
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    Validator validator;
+    // 표현식
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    // 프로퍼티
+    @Value("${my.value}")
+    int myValue;
+
+    // 표현식 내부의 프로퍼티 적용
+    @Value("#{${my.value} eq 100}")
+    boolean isMyValue100;
+
+    // Sample 빈의 data
+    @Value("#{sample.data}")
+    int data;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(validator.getClass());
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(hello);
+        System.out.println(myValue);
+        System.out.println(isMyValue100);
+        System.out.println(data);
 
-        // 테스트를 위한 유효하지 않는 값 입력
-        Event event = new Event();
-        event.setLimit(-1);
-        event.setEmail("abcde");
-        Errors errors = new BeanPropertyBindingResult(event, "event");
-
-        validator.validate(event, errors);
-        System.out.println(errors.hasErrors());
-
-        errors.getAllErrors().forEach(e -> {
-            System.out.println("===== error code =====");
-            Arrays.stream(e.getCodes()).forEach(System.out::println);
-            System.out.println(e.getDefaultMessage());
-        });
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("2 + 100");
+        Integer value = expression.getValue(Integer.class);
+        System.out.println(value);
     }
 }
---
+++ spring-study/src/main/java/dev/spring/framework/springstudy/SpringEL/Sample.java
@@ -0,0 +1,13 @@
+package dev.spring.framework.springstudy.SpringEL;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+
+    private int data = 300;
+
+    public int getData() {
+        return data;
+    }
+}

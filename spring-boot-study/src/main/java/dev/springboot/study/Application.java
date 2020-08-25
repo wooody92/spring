3cb7a7ae2a388c0cd745117c5cba1e9b70023402
wooody92 <pkower4@naver.com>
2020-08-25 17:34:28 +0900

    spring boot study - SpringApplication 1부

--- spring-boot-study/src/main/java/dev/springboot/study/Application.java
+++ spring-boot-study/src/main/java/dev/springboot/study/Application.java
@@ -1,12 +1,29 @@
 package dev.springboot.study;
 
+import java.io.PrintStream;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application {
 
+    /**
+     * SpringApplcation 실행 방법
+     */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        // 1 : static method
+//        SpringApplication.run(Application.class, args);
+//
+//        // 2 : instance
+//        SpringApplication app = new SpringApplication(Application.class);
+//        app.run(args);
+
+        // 3 : builder
+        new SpringApplicationBuilder()
+            .sources(Application.class)
+            .run(args);
     }
 }

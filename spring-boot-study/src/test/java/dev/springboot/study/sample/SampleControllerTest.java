0fa35948aa7e064f9779715c5cd73837bf42b187
wooody92 <pkower4@naver.com>
2020-08-30 20:09:28 +0900

    spring boot study - 테스트 : TestRestTemplate

--- spring-boot-study/src/test/java/dev/springboot/study/sample/SampleControllerTest.java
+++ spring-boot-study/src/test/java/dev/springboot/study/sample/SampleControllerTest.java
@@ -1,6 +1,7 @@
 package dev.springboot.study.sample;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,22 +13,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class SampleControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("hello henry"))
-            .andDo(print());
+        // 테스트 환경에서 mockSampleService.getName()이 호출되면 "wooody92"로 응답한다.
+        when(mockSampleService.getName()).thenReturn("wooody92");
+        String result = testRestTemplate.getForObject("/hello", String.class);
+        assertEquals("hello wooody92", result);
     }
 }

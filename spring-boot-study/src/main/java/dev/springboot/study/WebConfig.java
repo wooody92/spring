bedc0b8283e018f14648be2181f10f06f8e8b7ed
wooody92 <pkower4@naver.com>
2020-09-03 16:02:24 +0900

    spring boot study - 스피링 웹 MVC : 정적 리소스 지원

--- spring-boot-study/src/main/java/dev/springboot/study/WebConfig.java
+++ spring-boot-study/src/main/java/dev/springboot/study/WebConfig.java
@@ -1,10 +1,16 @@
 package dev.springboot.study;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/henry/")
+            .setCachePeriod(20);
+    }
 }

0b2ed2819ca04a0b5e3ccdd31aac3d168615339c
wooody92 <pkower4@naver.com>
2020-09-07 18:26:33 +0900

    spring boot study - 스피링 웹 MVC : CORS

--- spring-boot-study/src/main/java/dev/springboot/study/Application.java
+++ spring-boot-study/src/main/java/dev/springboot/study/Application.java
@@ -3,9 +3,19 @@ package dev.springboot.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @CrossOrigin(origins = "http://localhost:18080")
+    @GetMapping("/cors")
+    public String corsTest() {
+        return "hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
---
+++ spring-boot-study/src/main/java/dev/springboot/study/cors/WebConfig.java
@@ -0,0 +1,15 @@
+package dev.springboot.study.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/cors")
+            .allowedOrigins("http://localhost:18080");
+    }
+}

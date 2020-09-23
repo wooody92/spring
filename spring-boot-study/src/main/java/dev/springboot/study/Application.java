a26fbb9bb82f63250455b84adcda6ccc75db1e70
wooody92 <pkower4@naver.com>
2020-09-24 02:17:49 +0900

    spring boot study - 스프링 REST client : WebClient

--- spring-boot-study/src/main/java/dev/springboot/study/Application.java
+++ spring-boot-study/src/main/java/dev/springboot/study/Application.java
@@ -3,7 +3,12 @@ package dev.springboot.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @SpringBootApplication
 public class Application {
@@ -12,5 +17,20 @@ public class Application {
         SpringApplication app = new SpringApplication(Application.class);
 //        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
+    }
+
+//    @Bean
+//    public WebClientCustomizer webClientCustomizer() {
+//        return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
+//    }
+
+    @Bean
+    public RestTemplateCustomizer restTemplateCustomizer() {
+        return new RestTemplateCustomizer() {
+            @Override
+            public void customize(RestTemplate restTemplate) {
+                restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+            }
+        };
     }
 }
--- spring-boot-study/src/main/java/dev/springboot/study/SampleRunner.java
+++ spring-boot-study/src/main/java/dev/springboot/study/SampleRunner.java
@@ -7,27 +7,44 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
 
     @Autowired
-    RestTemplateBuilder restTemplateBuilder;
+    WebClient.Builder builder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        RestTemplate restTemplate = restTemplateBuilder.build();
+        WebClient webClient = builder.baseUrl("http://localhost:8080").build();
 
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
-        System.out.println(helloResult);
+        Mono<String> helloMono = webClient.get().uri("/hello")
+            .retrieve()
+            .bodyToMono(String.class);
+        helloMono.subscribe(s -> {
+            System.out.println(s);
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
 
-        String worldResult = restTemplate.getForObject("http://localhost:8080/world", String.class);
-        System.out.println(worldResult);
-
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+        Mono<String> worldMono = webClient.get().uri("/world")
+            .retrieve()
+            .bodyToMono(String.class);
+        worldMono.subscribe(s -> {
+            System.out.println(s);
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
     }
 }
